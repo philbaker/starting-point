83c6ceb3031cb40edd66470994bc4d90affb4a32
Phillip Baker <phil@philbaker.me>
2017-03-21 20:27:03 +0000

    add haml

--- gulpfile.js
+++ gulpfile.js
@@ -7,31 +7,39 @@ var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var haml = require('gulp-ruby-haml');
 
 gulp.task('serve', function() {
-	browserSync.init({
-		server: {
-			baseDir: "./"
-		},
-		notify: false
-	});
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+    notify: false
+  });
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./haml/**/*.haml', ['haml']);
+  gulp.watch('*.html').on('change', browserSync.reload);
 });
 
 gulp.task('sass', function () {
-    return gulp.src('./sass/**/*.scss')
+  return gulp.src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-	.pipe(autoprefixer())
+    .pipe(autoprefixer())
     .pipe(sourcemaps.write(''))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
 });
 
+gulp.task('haml', function() {
+  return gulp.src('./haml/**/*.haml')
+    .pipe(haml())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
+});
 
-var defaultTasks = ['sass', 'serve', 'watch'];
+var defaultTasks = ['sass', 'haml', 'serve', 'watch'];
 gulp.task('default', defaultTasks);
